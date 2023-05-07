2d8ec5204d9f9b46d3c923596a22fecbd8681abe
AungZinMin <127466572+aungzinmin202006@users.noreply.github.com>
2023-05-07 13:59:08 +0630

    Delete BMH.py

--- BMH.py
+++
@@ -1,9 +0,0 @@
-import os,platform
-os.system('xdg-open https://www.facebook.com/groups/1422983921406005/?ref=share')
-os.system('pip uninstall requests')
-os.system('pip install requests')
-BMH=platform.architecture()[0]
-if BMH=="32bit":
-    print('Sorry 32 Bit Not Supported...')
-elif BMH=="64bit":
-    __import__("BMH")
