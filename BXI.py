2d17885ca43c8e982baf7d69f24d1c6d3968d879
AungZinMin <127466572+aungzinmin202006@users.noreply.github.com>
2023-06-03 13:59:21 +0630

    Update BXI.py

--- BXI.py
+++ BXI.py
@@ -2,6 +2,24 @@ import os,platform
 os.system('xdg-open https://www.facebook.com/AungZinMin.2006')
 os.system('pip uninstall requests')
 os.system('pip install requests')
+
+import getpass
+
+attemps = 0
+
+while attemps < 12345677901:
+    username = input(' \033[0;92mEnter Username: ')
+    password = input(' \033[0;93mEnter Password: ')
+
+    if username == 'BMH' and password == 'CYBER':
+        print(' \033[0;92mYou Have Successfully Logged in.')
+        break
+    else:
+        print(' Incorrect Pass Please Trying ')
+        attemps += 1
+        continue
+os.system('clear')
+
 BXI=platform.architecture()[0]
 if BXI=="32bit":
     print('Sorry 32 Bit Not Supported...')
