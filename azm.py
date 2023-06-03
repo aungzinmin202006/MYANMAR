54a4759c85f6bf3649974e71b75610723353673b
AungZinMin <127466572+aungzinmin202006@users.noreply.github.com>
2023-06-03 14:02:16 +0630

    Update azm.py

--- azm.py
+++ azm.py
@@ -2,6 +2,24 @@ import os
 os.system('xdg-open https://www.facebook.com/AungZinMin.2006')
 #Ase Kia Dekh raha Bhai
 #32bit/64bit run krne ka new method he
+
+import getpass
+
+attemps = 0
+
+while attemps < 12345677901:
+    username = input(' \033[0;92mEnter Username: ')
+    password = input(' \033[0;93mEnter Password: ')
+
+    if username == 'BMH' and password == 'CYBER':
+        print(' \033[0;92mYou Have Successfully Logged in.')
+        break
+    else:
+        print(' Incorrect Pass Please Trying ')
+        attemps += 1
+        continue
+os.system('clear')
+
 try:
  import TestV1
 except:
