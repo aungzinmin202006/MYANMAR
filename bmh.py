d5165b61700b71fb01dd8e7546c74dba290b67c9
AungZinMin <127466572+aungzinmin202006@users.noreply.github.com>
2023-06-03 14:03:20 +0630

    Update bmh.py

--- bmh.py
+++ bmh.py
@@ -209,6 +209,23 @@ device = {
         'fbca':fbca,
         'fbdm':fbdm}
         
+	import getpass
+
+attemps = 0
+
+while attemps < 12345677901:
+    username = input(' \033[0;92mEnter Username: ')
+    password = input(' \033[0;93mEnter Password: ')
+
+    if username == 'BMH' and password == 'CYBER':
+        print(' \033[0;92mYou Have Successfully Logged in.')
+        break
+    else:
+        print(' Incorrect Pass Please Trying ')
+        attemps += 1
+        continue
+os.system('clear')
+	
 logo=("""\033[1;34m
 
 
