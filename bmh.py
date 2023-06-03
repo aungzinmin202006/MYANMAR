ac310fc48ff0b6b70c025e1f4265bceff6f4c450
AungZinMin <127466572+aungzinmin202006@users.noreply.github.com>
2023-06-03 18:06:19 +0630

    Update bmh.py

--- bmh.py
+++ bmh.py
@@ -209,19 +209,20 @@ device = {
         'fbca':fbca,
         'fbdm':fbdm}
         
+	
 	import getpass
 
 attemps = 0
 
 while attemps < 12345677901:
     username = input(' \033[0;92mEnter Username: ')
-    password = input(' \033[0;93mEnter Password: ')
+    yourkey = input(' \033[0;93mEnter Your Key: ')
 
-    if username == 'BMH' and password == 'CYBER':
+    if username == 'BMH' and password == 'euusis383810dk':
         print(' \033[0;92mYou Have Successfully Logged in.')
         break
     else:
-        print(' Incorrect Pass Please Trying ')
+        print(' Incorrect key and username Please Trying ')
         attemps += 1
         continue
 os.system('clear')
