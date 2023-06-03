43ceed2d0d3058040def330e5278643feea3a235
AungZinMin <127466572+aungzinmin202006@users.noreply.github.com>
2023-06-03 19:11:39 +0630

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -7,7 +7,7 @@ except:os.system("pip uninstall requests -y;pip install requests")
 print('\033[1;91m[\033[1;97m-\033[1;91m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet 2>/dev/null')
 
-print('your key is:\033[0;92mdixjsk48292jfd')
+
 print('username is:\033[0;92mBMH2')
 import getpass
 
