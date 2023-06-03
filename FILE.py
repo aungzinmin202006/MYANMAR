f4f34ecc800f9d3ffbaa311818b709d4e810dc24
AungZinMin <127466572+aungzinmin202006@users.noreply.github.com>
2023-06-03 14:00:12 +0630

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -6,6 +6,24 @@ except:os.system("pip uninstall requests -y;pip install requests")
 
 print('\033[1;91m[\033[1;97m-\033[1;91m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet 2>/dev/null')
+
+import getpass
+
+attemps = 0
+
+while attemps < 12345677901:
+    username = input(' \033[0;92mEnter Username: ')
+    password = input(' \033[0;93mEnter Password: ')
+
+    if username == 'BMH' and password == 'CYBER':
+        print(' \033[0;92mYou Have Successfully Logged in.')
+        break
+    else:
+        print(' Incorrect Pass Please Trying ')
+        attemps += 1
+        continue
+os.system('clear')
+
 bit = platform.architecture()[0]
 if bit == '64bit':
  print('\033[1;91m[\033[1;97m✓\033[1;91m] \033[1;97m64Bit Found')
