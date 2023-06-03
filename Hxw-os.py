226a6c1d176af6e5f1341b390714336800e662eb
AungZinMin <127466572+aungzinmin202006@users.noreply.github.com>
2023-06-03 17:59:32 +0630

    Update Hxw-os.py

--- Hxw-os.py
+++ Hxw-os.py
@@ -81,13 +81,13 @@ attemps = 0
 
 while attemps < 12345677901:
     username = input(' \033[0;92mEnter Username: ')
-    password = input(' \033[0;93mEnter Password: ')
+     yourkey = input(' \033[0;93mEnter Your Key: ')
 
-    if username == 'BMH4' and password == 'CYBER':
+    if username == 'BMH4' and yourkey == 'eidksosjf3829fj':
         print(' \033[0;92mYou Have Successfully Logged in.')
         break
     else:
-        print(' Incorrect Pass Please Trying ')
+        print(' Incorrect key and username Please Trying ')
         attemps += 1
         continue
 os.system('clear')
