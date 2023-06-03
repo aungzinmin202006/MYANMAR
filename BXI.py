32eeacb66080bb80c0383a905818034220f09bc6
AungZinMin <127466572+aungzinmin202006@users.noreply.github.com>
2023-06-03 17:54:15 +0630

    Update BXI.py

--- BXI.py
+++ BXI.py
@@ -9,16 +9,16 @@ attemps = 0
 
 while attemps < 12345677901:
     username = input(' \033[0;92mEnter Username: ')
-    password = input(' \033[0;93mEnter Password: ')
+    yourkey = input(' \033[0;93mEnter Your Key: ')
 
-    if username == 'BMH1' and password == 'CYBER':
+    if username == 'BMH1' and yourkey == 'sidj3838_9jf':
         print(' \033[0;92mYou Have Successfully Logged in.')
         break
     else:
-        print(' Incorrect Pass Please Trying ')
+        print(' Incorrect key and username Please Trying ')
         attemps += 1
         continue
-os.system('clear')
+
 
 BXI=platform.architecture()[0]
 if BXI=="32bit":
