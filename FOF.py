2ca046523cea26a764c2d3e907de2a987eb98366
AungZinMin <127466572+aungzinmin202006@users.noreply.github.com>
2023-06-07 07:55:38 +0630

    Update FOF.py

--- FOF.py
+++ FOF.py
@@ -11,7 +11,7 @@ while attemps < 12345677901:
     username = input(' \033[0;92mEnter Username: ')
     yourkey  = input(' \033[0;93mEnter Your Key: ')
 
-    if username == 'BMH3' and yourkey == '38103_cjdlahnck':
+    if username == 'BMH3' and yourkey == 'sj38-29cjdok':
         print(' \033[0;92mYou Have Successfully Logged in.')
         break
     else:
